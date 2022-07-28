8f28126c05d155deaf17775641c817251ca9f5a7
davecode2019 <dave.code2019@gmail.com>
2022-07-28 17:34:33 +0100

    Code commented

--- calculator.js
+++ calculator.js
@@ -1,16 +1,21 @@
 function add(x, y) {
+  // Function to add two numbers and return the result.
   return x + y;
 }
 
 function subtract(x, y) {
+  // Function to subtract two numbers and return the result.
   return x - y;
 }
 
 function multiply(x, y) {
+  // Function to multiply two numbers and return the result.
   return x * y;
 }
 
 function divide(x, y) {
+  // Function to divide two numbers and return the result.
+  // Provides feedback to user and reloads app if user attempts to divide by 0.
   if (y === 0) {
     alert("Illegal operation, cannot divide by 0");
     location.reload();
@@ -20,6 +25,7 @@ function divide(x, y) {
 }
 
 function operate(stack) {
+  // Takes an array and performs appropriate calculation actions based on contents.
   let x = Number(stack[0]);
   let y = Number(stack[2]);
   switch (stack[1]) {
@@ -41,6 +47,7 @@ function operate(stack) {
 }
 
 function updateCurrentNumbers(str) {
+  // Updates the user entered nuber string.
   if (currentNumbers === "0" || currentOperator === "=") {
     currentNumbers = str;
     updateNumberDisplay();
@@ -50,20 +57,25 @@ function updateCurrentNumbers(str) {
     updateNumberDisplay();
   }
 }
+
 function updateNumberDisplay() {
+  // Updates the number display of the app.
   numberDisplay.innerHTML = currentNumbers;
 }
 
 function expo(x, f) {
+  // Converts calculation results to exponential format if too long for the app display.
   return Number.parseFloat(x).toExponential(f);
 }
 
 function updateOperatorDisplay(operator) {
+  // Updates the operator display of the app.
   currentOperator = operator;
   operatorDisplay.innerHTML = currentOperator;
 }
 
 function operatorPress(operator) {
+  // Processes user presses of operator buttons.
   updateOperatorDisplay(operator);
   if (stack.length === 2) {
     if (operator === "=") {
@@ -78,6 +90,7 @@ function operatorPress(operator) {
 }
 
 function performOperation() {
+  // Performs operations once enough information has been provided and arranges display of results.
   stack.push(currentNumbers);
   currentNumbers = String(operate(stack));
   if (currentNumbers.length <= 15) {
@@ -90,12 +103,14 @@ function performOperation() {
 }
 
 function updateStack(operator) {
+  // Updates the stack array as required
   stack.push(currentNumbers);
   currentNumbers = "0";
   stack.push(operator);
 }
 
 function decimalPress() {
+  // Allows the use of decimals in user entered numbers. Only one decimal can be added
   if (!currentNumbers.includes(".")) {
     currentNumbers += ".";
     updateNumberDisplay();
@@ -103,6 +118,7 @@ function decimalPress() {
 }
 
 function deletePress() {
+  // Allows user to delete mistyped input.
   if (currentNumbers.length > 1) {
     currentNumbers = currentNumbers.slice(0, currentNumbers.length - 1);
     updateNumberDisplay();
